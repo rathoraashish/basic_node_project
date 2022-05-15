d7b7d1a35cda75446b909b43a114f182386242e5
Ashish <rathorashish79@gmail.com>
2022-05-16 00:38:50 +0530

    body parser module added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const router = express.Router();
 const User = require('../controllers/user');
 const responseMessage = require('../utils/responseMessage');
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', (req, res) => {
     var user = "Ashish";
@@ -16,7 +23,7 @@ router.get('/about', (req, res) => {
 
 router.post('/addUsers',async (req,res)=>{
     try {
-        console.log("Data from postman",req.data);
+        console.log("Data from postman",req.body);
         const data = await User.addUser(req)
         console.log("data:response)__", data);
         return responseMessage(null, data)
