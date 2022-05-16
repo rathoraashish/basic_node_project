4be54983d339859179a46d8e413755ee6f6b2976
Ashish <rathorashish79@gmail.com>
2022-05-16 23:03:16 +0530

    Update user API added

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const User = require('../controllers/user');
 const responseMessage = require('../utils/responseMessage');
 
 // support parsing of application/json type post data
-app.use(bodyParser.json());
+router.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,16 +21,51 @@ router.get('/about', (req, res) => {
     res.render('about', {user:user});
 })
 
-router.post('/addUsers',async (req,res)=>{
+// -------------Post request for insert user--------------------
+
+router.post('/addUser',async (req,res)=>{
     try {
-        console.log("Data from postman",req.body);
+        // console.log("Data from postman",req.body);
         const data = await User.addUser(req)
         console.log("data:response)__", data);
+        res.send({
+          "status":200,
+          "message":"User inserted successfuly",
+          "data":req.body
+        })
         return responseMessage(null, data)
       } catch (err) {
         console.log("Error::index.js))", err);
+        res.send({
+          "status":400,
+          "message":"failed to insert user",
+          "data":err
+        })
         return responseMessage(err, null)
       }
 })
 
+// -------------Post request for update user--------------------
+
+router.post('/updateUser',async (req,res)=>{
+  try {
+      // console.log("Data from postman",req.body);
+      const data = await User.updateUser(req)
+      console.log("data:response)__", data);
+      res.send({
+        "status":200,
+        "message":"User updated successfuly",
+        "data":req.body
+      })
+      return responseMessage(null, data)
+    } catch (err) {
+      console.log("Error::index.js))", err);
+      res.send({
+        "status":400,
+        "message":"failed to updated user",
+        "data":err
+      })
+      return responseMessage(err, null)
+    }
+})
 module.exports = router;
